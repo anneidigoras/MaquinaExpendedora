4b8b1a5c5d90628e93f04d1fa50a564eabdde8d1
Anne <Anne@10.207.76.192>
2016-10-25 13:54:24 +0200

    Primera Version

---
+++ MaquinaExpendedora/src/clsAdministrador.java
@@ -0,0 +1,34 @@
+
+public class clsAdministrador 
+{
+	
+	private String contraseña;
+	private String nombre;
+	
+	clsAdministrador()
+	{
+		contraseña="";
+		nombre="";
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
+
+}
